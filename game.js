dabd20463ca0279efcbbc5b66b3fdfadb4a73265
kahlil29 <kahlil29@gmail.com>
2015-01-08 20:19:48 +0530

    Added function move_down
    this function moves the "square" down through the very small grid that
    really needs to become bigger

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ function Grid () {
 function Shape () {
 	this.shape_sqr = [[true,true],
 						[true,true]];
+	this.size_sqr = 2;
 
 
 } 
@@ -15,12 +16,14 @@ function Shape () {
 var grid = new Grid();
 var shape = new Shape();
 
-window.alert(grid.dimension[0]+"\n"+grid.dimension[1]+"\n"+grid.dimension[2]+"\n"+grid.dimension[3]);
+//window.alert(grid.dimension[0]+"\n"+grid.dimension[1]+"\n"+grid.dimension[2]+"\n"+grid.dimension[3]);
 
 display();
 
 window.alert(grid.dimension[0]+"\n"+grid.dimension[1]+"\n"+grid.dimension[2]+"\n"+grid.dimension[3]);
 
+move_down(0); 
+move_down(1);
 
 function display(xaxis,yaxis) {
 	for( var i = 0; i < 2; i++ ){
@@ -28,4 +31,25 @@ function display(xaxis,yaxis) {
 			grid.dimension[i][j] = shape.shape_sqr[i][j];
 		}
 	}
+}
+
+function move_down (arg1) {
+	for (var i = arg1; i < arg1+ shape.size_sqr; i++){
+		for (var j = 0; j < shape.size_sqr;j++) {
+				grid.dimension[i][j] = false;
+		}
+	}
+	arg1 +=1;
+	var temp = arg1;
+	var k = 0; 
+	for (var i = arg1; i < arg1+ shape.size_sqr; i++, k++){
+		for (var j = 0; j < shape.size_sqr;j++) {
+				grid.dimension[i][j] = shape.shape_sqr[k][j];
+		}
+	}
+	window.alert(grid.dimension[0]+"\n"+grid.dimension[1]+"\n"+grid.dimension[2]+"\n"+grid.dimension[3]);
+	//return move_down(arg1);
+	// if(temp<= shape.shape_sqr){
+	// 	return move_down(temp);
+	// }
 }
