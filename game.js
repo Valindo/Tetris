333a9d85e2d3cad1074dfec0ab70e0b5d0063e89
valindo godinho <valindo.godinho@gmail.com>
2015-01-08 16:13:55 +0530

    Created the index file Created the Javascript file Added the Grid class Added the Shape class Wrote a function to display The Grid and position of the object.

---
+++ game.js
@@ -0,0 +1,31 @@
+function Grid () {
+	this.dimension = [[false,false,false,false],
+						[false,false,false,false],
+						[false,false,false,false],
+						[false,false,false,false]];	
+}
+
+function Shape () {
+	this.shape_sqr = [[true,true],
+						[true,true]];
+
+
+}
+
+var grid = new Grid();
+var shape = new Shape();
+
+window.alert(grid.dimension[0]+"\n"+grid.dimension[1]+"\n"+grid.dimension[2]+"\n"+grid.dimension[3]);
+
+display();
+
+window.alert(grid.dimension[0]+"\n"+grid.dimension[1]+"\n"+grid.dimension[2]+"\n"+grid.dimension[3]);
+
+
+function display(xaxis,yaxis) {
+	for( var i = 0; i < 2; i++ ){
+		for ( var j = 0; j < 2; j++ ){
+			grid.dimension[i][j] = shape.shape_sqr[i][j];
+		}
+	}
+}
